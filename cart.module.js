9c50a0cd233f7542c14c62647b15da1466f57fdb
Zerocomputer <staryliss.nikita.2004@gmail.com>
2023-06-02 04:30:58 +0500

    Обновление! - классы переведены в атрибуты; - добавлен атрибут "тип"; - добавлены кастомные условия добавления товаров.

--- cart.module.js
+++ cart.module.js
@@ -1,7 +1,8 @@
 (() => {
     // Cart
-    var cart = [];
-    var cartWindow = document.getElementById('cart-window') ?? null;
+    window.cart = [];
+    window.cartPayments = [];
+    window.cartWindow = document.getElementById('cart-window') ?? null;
 
     // Loading cart from local storage 
     if (localStorage.getItem('cart_data')) {
@@ -9,6 +10,24 @@
             cart = JSON.parse(localStorage.getItem('cart_data'));
         } catch (e) {
             console.log(`Appeared error while parsing cart JSON: ${e}`);
+        }
+    }
+
+    // Reload payment buttons
+    const reloadPayment = () => {
+        let cartIsEmpty = cart.length === 0;
+
+        for (let paymentButton of cartPayments) {
+            let hideMethod = paymentButton.getAttribute('zero-cart-hide-if-empty') === "" ? true : false;
+
+            if (hideMethod) {
+                if (cartIsEmpty)
+                    paymentButton.setAttribute('style', "display: none !important;");
+                else
+                    paymentButton.removeAttribute('style');
+            } else {
+                paymentButton.disabled = cartIsEmpty;
+            }
         }
     }
 
@@ -21,10 +40,19 @@
 
         for (let product of cart) {
             let productElement = document.createElement('div');
+            productElement.className = 'border rounded p-4 mb-2 d-flex align-items-center'
+            productElement.style.gap = '16px';
             productElement.innerHTML = `
                 <img src="${product.image}" alt="Placeholder image">
-                <h1>${product.name}</h1>
-                <p>${product.price} x ${product.count}</p>
+                <div>
+                    <h3>${product.name}</h3>
+                    <p>${product.price} x ${product.count}</p>
+                </div>
+                <button 
+                    class="btn btn-danger ms-auto" 
+                    zero-do-cart-remove 
+                    zero-cart-id="${product.id}" 
+                    zero-cart-name="${product.name}">X</button>
             `;
 
             cartWindow.appendChild(productElement);
@@ -34,7 +62,7 @@
             amountElement.textContent = getAmount();
         }
 
-        for (let addToCartElement of document.querySelectorAll('.add_to_cart')) {
+        for (let addToCartElement of document.querySelectorAll('[zero-do-cart-add]')) {
             if (addToCartElement.getAttribute('listen')) {
                 continue;
             }
@@ -42,11 +70,19 @@
             addToCartElement.setAttribute('listen', true);
 
             addToCartElement.addEventListener('click', (e) => {
-                add(getRequiredAttrsFromElement(addToCartElement));
+                let additionalStatementResult = true;
+                
+                if (e.target.getAttribute('zero-cart-additional-statement')) {
+                    let additionalStatement = e.target.getAttribute('zero-cart-additional-statement');
+                    additionalStatementResult = window[additionalStatement]();
+                }
+
+                if (additionalStatementResult)
+                    add(getRequiredAttrsFromElement(addToCartElement));
             });
         }
 
-        for (let removeFromCartElement of document.querySelectorAll('.remove_from_cart')) {
+        for (let removeFromCartElement of document.querySelectorAll('[zero-do-cart-remove]')) {
             if (removeFromCartElement.getAttribute('listen')) {
                 continue;
             }
@@ -57,6 +93,9 @@
                 remove(getRequiredAttrsFromElement(removeFromCartElement));
             });
         }
+
+        // Reloading payment buttons
+        reloadPayment();
     }
     // Saving cart to local storage
     const save = () => {
@@ -69,11 +108,11 @@
         }
     }
     // Adding product to cart
-    const add = ({id, name, price, image}) => {
+    const add = ({id, type, name, price, image}) => {
         let product = cart.filter((x) => x.id === id || x.name === name);
         
         if (product.length === 0) {
-            cart.push({id, name, price, image, count: 1});
+            cart.push({id, type, name, price, image, count: 1});
         } else {
             cart[cart.indexOf(product[0])].count++;
         }
@@ -103,6 +142,7 @@
     const getRequiredAttrsFromElement = (element) => {
         return {
             id: element.getAttribute('zero-cart-id'),
+            type: element.getAttribute('zero-cart-type'),
             name: element.getAttribute('zero-cart-name'),
             price: element.getAttribute('zero-cart-price'),
             image: element.getAttribute('zero-cart-image'),
@@ -121,6 +161,10 @@
 
     // Initialization
     document.addEventListener('DOMContentLoaded', () => {
+        // Loading all payments button
+        cartPayments = document.querySelectorAll('[zero-do-cart-payment]');
+
+        // Reloading UI
         reload();
     });
 })();
